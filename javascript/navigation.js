fd3becc939d3f1db0d4c704aa4926bf09718db6a
Vadim Kiselev <vadim8kiselev@gmail.com>
2018-03-10 04:28:45 +0400

    Template of texts

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,21 +1,21 @@
 let colors = {
     "welcome": {"header": "#6299ff", "body": "#e0eafb"},
     "skills": {"header": "#fb3668", "body": "#fbe0ea"},
-    "community": {"header": "#00c137", "body": "#c4ffdb"},
-    "management": {"header": "#b67bff", "body": "#f2e9fe"}
+    "speaking": {"header": "#00c137", "body": "#c4ffdb"},
+    "community": {"header": "#b67bff", "body": "#f2e9fe"}
 };
 
 let texts = {
-    "welcome": "Welcome",
-    "skills": "Skills",
-    "community": "Community",
-    "management": "Management"
+    "welcome": welcome,
+    "skills": skills,
+    "speaking": speaking,
+    "community": community
 };
 
 let pages = {
     "welcome": "skills",
-    "skills": "community",
-    "community": "management"
+    "skills": "speaking",
+    "speaking": "community"
 };
 
 function navigate(page) {
@@ -56,13 +56,13 @@ function updateContent(page) {
 }
 
 function updateText(page) {
-    $('.body-text').text(texts[page]);
+    $('.body-text').html(texts[page]);
 }
 
 function updateNavigation(page) {
     if (pages.hasOwnProperty(page)) {
         $('.body-navigation div').attr("onclick", "navigate('" + pages[page] + "');");
     } else {
-        window.location.replace("https://t.me/joinchat/A8zYJFK9sIwLiqB1UTZ91w");
+        $('.body-navigation div').attr("onclick", "window.location.replace('https://t.me/joinchat/A8zYJFK9sIwLiqB1UTZ91w');");
     }
 }
